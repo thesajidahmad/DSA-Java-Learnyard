51eac542aca1cff0a42f9f974567942af05a9160
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2025-05-07 18:50:36 +0530

    Larger in three Numbers

---
+++ Introduction to JAVA/file09_if_else_LargerInThree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class file09_if_else_LargerInThree {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if(a > b && b > c){
+            System.out.println(a);
+        }else if(a < b && b > c){
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
+        scan.close();
+    }
+}
